65d8c48d0f59771cb22e04a77f85512e1d36aae0
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-02-27 16:54:53 +0000

    Computed the basic actions of the shoe controller

---
+++ app/controllers/shoes_controller.rb
@@ -0,0 +1,36 @@
+class ShoesController < ApplicationController
+  def index
+    @shoes = Shoe.all
+  end
+
+  def show
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def new
+    @shoe = Shoe.new
+  end
+
+  def create
+    @shoe = Shoe.create(shoe_params)
+    if @shoe.save
+      redirect_to shoe_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def update
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def destroy
+    @shoe = Shoe.find(params[:id])
+    @shoe.destroy
+    redirect_to shoe_path, status: :see_other
+  end
+end
