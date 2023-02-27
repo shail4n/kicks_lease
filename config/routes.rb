155bec98a449cbb780682c5a1bbd8628b042b425
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-02-27 17:11:33 +0000

    Prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-  get 'shoes/index'
-  get 'shoes/show'
-  get 'shoes/new'
-  get 'shoes/create'
-  get 'shoes/edit'
-  get 'shoes/update'
-  get 'shoes/destroy'
-  devise_for :users
-  root to: "shoes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :shoes do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [:index, :show, :destroy]
+  resources :bookings, only: [ :index, :show, :destroy ]
 end
