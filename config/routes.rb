21d77e4ade431b489b0c3df980f0a2282c78e6e8
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-02-28 12:11:53 +0000

    stash

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
   resources :shoes do
     resources :bookings, only: [ :new, :create ]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_102018) do
+=======
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_100815) do
+>>>>>>> 72b8acde385dd5f871daa7c1ed6961a5b92e53bf
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
